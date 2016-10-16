6a068c8d63f79f8eb6d8f4a06a2602067069f46a
Maciej Sitko <maciej@neatcube.co.uk>
2016-10-16 21:14:50 +0100

    progress with works

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,7 @@
 import Html exposing (Html, div, button)
 import Html.Events exposing (onClick)
 import Html.App as App
+import VirtualDom exposing (Node)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
@@ -51,27 +52,29 @@ view: Model -> Html Msg
 view model =
       let
         angle =
-          [ turns (Time.inMinutes model.time)
-           , turns (Time.inHours model.time) ]
+          { sec = turns (Time.inMinutes model.time)
+          , min = turns (Time.inHours model.time)
+          }
       in
         div [] [
             svg [ viewBox "0 0 100 100", width "300px" ]
               [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-              , (List.map drawLines angle) ]
-            ,
-            button [ onClick Toggle ] [ text "Toggle" ]]
+              , drawLines angle.sec model.playing
+              , drawLines angle.min model.playing
+              ]
+            , button [ onClick Toggle ] [ text "Toggle" ]
+            ]
 
 --SVG
 
-drawLines: Float -> Html Msg
-drawLines angle =
-    let
-        handX =
-          toString (50 + 40 * cos angle)
-
-        handY =
-          toString (50 + 40 * sin angle)
+drawLines: Float -> Bool -> Node a
+drawLines angle play =
+    let ( handX, handY ) =
+          ( toString (50 + 40 * cos angle), toString (50 + 40 * sin angle) )
     in
-       line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+       if play == True then
+         line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+       else
+          line [] []
 
 
