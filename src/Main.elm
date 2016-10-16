0aafc94b43e7ed0fc55e66976f2a4eca5bf919a1
Maciej Sitko <maciej@neatcube.co.uk>
2016-10-16 15:52:10 +0100

    Init

--- src/Main.elm
+++ src/Main.elm
@@ -1,113 +1,37 @@
-import Html exposing (..)
+import Html exposing (Html)
 import Html.App as App
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
-import Http
-import Json.Decode as Json
-import Task
-import Set exposing (Set)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Time exposing (Time, second)
 
 
 main =
   App.program
-    { init = init "sexy ass"
+    { init = init
     , view = view
     , update = update
     , subscriptions = subscriptions
     }
 
+--MODEL
 
+type alias Model = Time
 
--- MODEL
+init : (Model, Cmd Msg)
+init =
+  (0, Cmd.none)
 
+--UPDATE
 
-type alias Model =
-  { topic : String
-  , gifUrl : String
-  , error : String
-  , history : Set String
-  }
+type Msg = Tick Time
 
-
-init : String -> (Model, Cmd Msg)
-init topic =
-  ( Model topic "waiting.gif" "" (Set.fromList [topic])
-  , getRandomGif topic
-  )
-
-
-
--- UPDATE
-
-
-type Msg
-  = MorePlease
-  | FetchSucceed String
-  | FetchFail Http.Error
-  | UpdateTopic String
-
-
-update : Msg -> Model -> (Model, Cmd Msg)
+update: Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  case msg of
-    MorePlease ->
-      ({model | history = (Set.insert model.topic model.history) }, getRandomGif model.topic)
-
-    FetchSucceed newUrl ->
-      (Model model.topic newUrl "" model.history, Cmd.none)
-
-    FetchFail _ ->
-      (Model model.topic "" "Fetch error!" model.history, Cmd.none)
-
-    UpdateTopic newTopic ->
-      ({model | topic = newTopic }, Cmd.none)
+    case msg of
+        Tick Time ->
+            (model, Cmd.none)
 
 
--- VIEW
+--VIEW
 
-
-view : Model -> Html Msg
-view model =
-  div []
-    [ h2 [] [text model.topic]
-    , button [ onClick MorePlease ] [ text "More Please!" ]
-    , input [ onInput UpdateTopic ] []
-    , br [] []
-    , select []
-      (List.map toOption (Set.toList model.history))
-    , img [src model.gifUrl] []
-    , p [] [text model.error]
-    ]
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-  Sub.none
-
-
-
--- HTTP
-
-
-getRandomGif : String -> Cmd Msg
-getRandomGif topic =
-  let
-    url =
-      "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
-  in
-    Task.perform FetchFail FetchSucceed (Http.get decodeGifUrl url)
-
-
-decodeGifUrl : Json.Decoder String
-decodeGifUrl =
-  Json.at ["data", "image_url"] Json.string
-
--- DOM
-
-toOption: String -> Html msg
-toOption topic =
-   option [] [text topic]
+--SUBSCRIPTIONS
