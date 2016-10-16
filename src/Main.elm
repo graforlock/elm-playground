c9529e76f408556bcd6b9d9575579c6c44b5e970
Maciej Sitko <maciej@neatcube.co.uk>
2016-10-16 20:48:13 +0100

    Solving the issue

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,5 @@
-import Html exposing (Html)
+import Html exposing (Html, div, button)
+import Html.Events exposing (onClick)
 import Html.App as App
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
@@ -15,23 +16,67 @@ main =
 
 --MODEL
 
-type alias Model = Time
+type alias Model = { time: Time, playing: Bool }
 
 init : (Model, Cmd Msg)
 init =
-  (0, Cmd.none)
+  (Model 0 True, Cmd.none)
 
 --UPDATE
 
-type Msg = Tick Time
+type Msg
+    = Tick Time
+    | Toggle
 
 update: Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        Tick Time ->
-            (model, Cmd.none)
+        Tick newTime ->
+            (Model newTime model.playing, Cmd.none)
+
+        Toggle ->
+            ({ model | playing = not(model.playing) }, Cmd.none)
+
+--SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every second Tick
+
 
 
 --VIEW
 
---SUBSCRIPTIONS
+view: Model -> Html Msg
+view model =
+      let
+        angle =
+          [ turns (Time.inMinutes model.time)
+           , turns (Time.inHours model.time) ]
+      in
+        div [] [
+            svg [ viewBox "0 0 100 100", width "300px" ]
+              [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+              ,
+              if model.playing == True then
+                (List.map drawLines angle)
+              else
+                line [] []
+              ]
+            , button [ onClick Toggle ] [ text "Toggle" ]
+        ]
+
+--SVG
+
+drawLines: List Time -> Html
+drawLines angle =
+    let
+        handX =
+          toString (50 + 40 * cos angle)
+
+        handY =
+          toString (50 + 40 * sin angle)
+    in
+       line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+
+
